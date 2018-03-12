ddaf83db5cab1fd23122ddd1d7af5aa09cfe32a2
Timur Babich <timur.babich@gmail.com>
2018-03-12 20:22:36 +0300

    edit P0341

--- P0341_SQLite/app/src/main/java/com/example/tbabich/p0341_sqlite/MainActivity.java
+++ P0341_SQLite/app/src/main/java/com/example/tbabich/p0341_sqlite/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.tbabich.p0341_sqlite;
 
 import android.content.ContentValues;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnAdd, btnRead, btnClear;
     EditText etName, etEmail;
@@ -43,17 +45,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         SQLiteDatabase database = dbHelper.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
 
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btnAdd:
                 contentValues.put(DBHelper.KEY_NAME, name);
                 contentValues.put(DBHelper.KEY_EMAIL, email);
                 database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
                 break;
             case R.id.btnRead:
+                Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+                    int emailIndex = cursor.getColumnIndex(DBHelper.KEY_EMAIL);
+
+                    do {
+                        Log.d("myLogs", cursor.getInt(idIndex) + ", " + cursor.getString(nameIndex) + ", " + cursor.getString(emailIndex));
+                    }
+                    while (cursor.moveToNext());
+                }
+                cursor.close();
                 break;
             case R.id.btnClear:
+                database.delete(DBHelper.TABLE_CONTACTS, null, null);
                 break;
         }
-
+    dbHelper.close();
     }
 }
